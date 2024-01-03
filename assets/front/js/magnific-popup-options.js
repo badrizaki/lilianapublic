354e090616b30ed32fef273eb8d459c2cd3d9aa5
Badri Zaki <badrizaki@gmail.com>
2024-01-03 17:10:37 +0700

    gallery video

--- assets/front/js/magnific-popup-options.js
+++ assets/front/js/magnific-popup-options.js
@@ -1,32 +1,66 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // MagnificPopup
-  var magnifPopup = function() {
-    $('.popup-image').magnificPopup({
-      type: 'image',
+  var magnifPopup = function () {
+    console.log("this", this);
+    $(".popup-image").magnificPopup({
+      // type: this.type,
+      // type: "iframe",
+      type: "image",
       removalDelay: 300,
-      mainClass: 'mfp-with-zoom',
-      gallery:{
-        enabled:true
+      mainClass: "mfp-with-zoom",
+      gallery: {
+        enabled: true,
       },
       zoom: {
         enabled: true, // By default it's false, so don't forget to enable it
 
         duration: 300, // duration of the effect, in milliseconds
-        easing: 'ease-in-out', // CSS transition easing function
+        easing: "ease-in-out", // CSS transition easing function
 
-        // The "opener" function should return the element from which popup will be zoomed in
-        // and to which popup will be scaled down
-        // By defailt it looks for an image tag:
-        opener: function(openerElement) {
-        // openerElement is the element on which popup was initialized, in this case its <a> tag
-        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-        return openerElement.is('img') ? openerElement : openerElement.find('img');
-        }
-      }
+        opener: function (openerElement) {
+          return openerElement.is("img")
+            ? openerElement
+            : openerElement.find("img");
+        },
+      },
+      callbacks: {
+        elementParse: function (item) {
+          // the class name
+          if (item.el.context.className == "popup-image video-link") {
+            item.type = "iframe";
+          } else {
+            item.type = "image";
+          }
+        },
+      },
     });
   };
-  
-  // Call the functions 
+
+  // Call the functions
   magnifPopup();
 
+  // $(".popup-image").magnificPopup({
+  //   delegate: "a",
+  //   type: "image",
+  //   tLoading: "Loading image #%curr%...",
+  //   mainClass: "mfp-img-mobile",
+  //   gallery: {
+  //     enabled: true,
+  //     navigateByImgClick: true,
+  //     preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+  //   },
+  //   image: {
+  //     tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+  //   },
+  //   callbacks: {
+  //     elementParse: function (item) {
+  //       // the class name
+  //       if (item.el.context.className == "video-link") {
+  //         item.type = "iframe";
+  //       } else {
+  //         item.type = "image";
+  //       }
+  //     },
+  //   },
+  // });
 });
